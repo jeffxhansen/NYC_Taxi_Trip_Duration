50fe2742433a1143f783676491579b9b8e5308bb
Jeff Hansen <jeffxhansen@gmail.com>
2023-10-26 09:49:42 -0600

    data_manager imports from config fine for jeff.ipynb

--- py_files/data_manager.py
+++ py_files/data_manager.py
@@ -8,6 +8,8 @@ def get_data():
     personal_health_df = pd.read_csv(f"{data_path}/personal_health_data.csv")
     digital_interaction_df = pd.read_csv(f"{data_path}/digital_interaction_data.csv")
     
+    return activity_environment_df, personal_health_df, digital_interaction_df
+    
     # merge the dataframes
     merged_df = None
     
